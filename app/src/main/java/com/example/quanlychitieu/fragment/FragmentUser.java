7e8dae0f9434568e6c8026ec2b527794adc4c447
bigqsys-android <thanhnampresident@gmail.com>
2020-07-01 22:19:25 +0700

    'namnt'

--- app/src/main/java/com/example/quanlychitieu/fragment/FragmentUser.java
+++ app/src/main/java/com/example/quanlychitieu/fragment/FragmentUser.java
@@ -10,8 +10,12 @@ import com.example.quanlychitieu.R;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
 
 public class FragmentUser extends Fragment {
+    private LoginButton loginButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
